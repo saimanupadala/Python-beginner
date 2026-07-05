a11a1e0ec1bd7494619f6b5315880250796332ab
saimanupadala <saimanupadala@gmail.com>
2026-07-05 14:05:29 +0530

    Add files via upload

---
+++ program 9.py
@@ -0,0 +1,17 @@
+import random
+
+choices = ["rock", "paper", "scissors"]
+
+computer = random.choice(choices)
+user = input("Enter rock, paper, or scissors: ").lower()
+
+print("Computer:", computer)
+
+if user == computer:
+    print("It's a tie!")
+elif (user == "rock" and computer == "scissors") or \
+     (user == "paper" and computer == "rock") or \
+     (user == "scissors" and computer == "paper"):
+    print("You win!")
+else:
+    print("You lose!")
